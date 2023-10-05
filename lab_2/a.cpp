95add9812390aacce9394f93964a1b6b12703c5a
Zhanserkenov Arsen <73212575+Zhanserkenov@users.noreply.github.com>
2023-10-05 14:42:47 +0600

    Create a.cpp

---
+++ lab_2/a.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+ 
+#define ll long long
+#define ld long double`
+#define ull unsigned ll
+#define ioi exit(0);
+ 
+#define inf (int)1e9+7
+ 
+#define F first
+#define S second
+ 
+#define pb push_back
+#define ppb pop_back
+ 
+#define lb(x) lower_bound(x)
+#define ub(x) upper_bound(x)
+ 
+#define sz(x) x.size()
+ 
+#define all(x) x.begin(),x.end()
+ 
+#define NFS ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0);
+ 
+using namespace std;
+
+const int N = 1e6 + 7;
+
+int a[N];
+
+int main(){
+	
+	NFS
+	
+	int n;
+	cin >> n;
+	
+	for(int i = 1; i <= n; i++){
+		cin >> a[i];
+	}
+	
+	int k;
+	cin >> k;
+	
+	int mn = inf, ans = -1;
+	for(int i = 1; i <= n; i++){
+		int sum = abs(k - a[i]);
+		if(mn > sum){
+			mn = sum;
+			ans = i - 1;
+		}
+	}
+	
+	cout << ans;
+	
+	ioi
+}
